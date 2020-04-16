e27c514b0b9021c9c36bf4fd07d6f6615d9090eb
Mattia Zanella <mattia_zanella@icloud.com>
2020-04-16 16:26:03 +0200

    Added MAMP options in constants

--- nuxt/constants/index.js
+++ nuxt/constants/index.js
@@ -25,4 +25,31 @@ export default {
       return `${ this.protocol }://${ this.host }${ port }/${ this.path_API }`
     },
   },
+  MAMP: {
+    protocol: 'http',
+    host: 'localhost',
+    port: 8888,
+    basePath: 'TEDx-Schio',
+    path_API: 'wordpress/wp-json/wp/v2',
+    SEO: {
+      title: 'TEDx Schio',
+      description: 'Schio rappresenta da sempre un centro industriale e culturale di notevole spessore e occupa una posizione di forte rilievo all’interno del contesto sociale del territorio.'
+    },
+    /**
+     * @description Thanks to IBM, defining URL structure by envs.
+     * @see https://www.ibm.com/support/knowledgecenter/SSGMCP_5.2.0/com.ibm.cics.ts.internet.doc/topics/dfhtl_uricomp.html
+     */
+    getFullPath() {
+      const port = this.port ? `:${this.port}` : ''
+      return `${ this.protocol }://${ this.host }${ port }/${this.basePath}`
+    },
+    /**
+     * @description Thanks to IBM, defining URL structure by envs.
+     * @see https://www.ibm.com/support/knowledgecenter/SSGMCP_5.2.0/com.ibm.cics.ts.internet.doc/topics/dfhtl_uricomp.html
+     */
+    getFullAPIPath() {
+      const port = this.port ? `:${this.port}` : ''
+      return `${ this.protocol }://${ this.host }${ port }/${this.basePath}/${ this.path_API }`
+    },
+  },
 }
