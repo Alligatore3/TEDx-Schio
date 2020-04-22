f27aeb2a1ba134c363f6a6fb574aa6f7b78b7eb8
Mattia Zanella <mattia_zanella@icloud.com>
2020-04-22 17:39:12 +0200

    Added BottomMatrix

---
+++ nuxt/components/BottomMatrix.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+    <div class="columns is-desktop is-multiline is-gapless m-0">
+      <div class="column is-half-desktop">
+        <figure class="image">
+          <img :src="matrix.lt.image" :alt="matrix.lt.alt" >
+        </figure>
+      </div>
+      <div class="column is-half-desktop">
+        <h2 class="ted-red title is-4 mt-1 px-1">
+          Cos'è TED?
+        </h2>
+        <div v-html="matrix.rt.text"></div>
+      </div>
+    </div>
+    <div class="columns is-desktop is-multiline is-gapless m-0 reverse-columns-mobile">
+      <div class="column is-half-desktop">
+        <h2 class="ted-red title is-4 mt-1 px-1">
+          Cos'è TEDxSchio?
+        </h2>
+        <div v-html="matrix.lb.text"></div>
+      </div>
+      <div class="column is-half-desktop">
+        <figure class="image">
+          <img :src="matrix.rb.image" :alt="matrix.rb.alt" >
+        </figure>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "BottomMatrix",
+    props: {
+      matrix: {
+        type: Object,
+        default: () => ({})
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
--- nuxt/pages/index.vue
+++ nuxt/pages/index.vue
@@ -8,6 +8,7 @@
         <SpeakersGrid :year="getCurrentEdition" />
       </div>
     </section>
+    <BottomMatrix :matrix="matrix" />
   </div>
 </template>
 
@@ -20,17 +21,32 @@ export default {
   components: {
     Hero: () => import('@/components/common/Hero'),
     Announcer: () => import('@/components/Announcer'),
+    BottomMatrix: () => import('@/components/BottomMatrix'),
     SpeakersGrid: () => import('@/components/common/SpeakersGrid/index'),
   },
   computed: {
     ...mapGetters('application', ['getCurrentEdition', 'getPageBySlugFromVUEX']),
+    matrix() {
+      return {
+        lt: {
+          image: this.homepageACFMetaBy('immagine_sx'),
+          alt: 'TED image'
+        },
+        lb: { text: this.homepageACFMetaBy('tedxschio') },
+        rt: { text: this.homepageACFMetaBy('ted') },
+        rb: {
+          image: this.homepageACFMetaBy('immagine_dx'),
+          alt: 'TED image'
+        },
+      }
+    }
   },
   methods: {
     ...mapMutations('application', ['PUSH_A_PAGE']),
     homepageACFMetaBy(key) {
       return this.getPageBySlugFromVUEX('homepage') &&
         this.getPageBySlugFromVUEX('homepage').acf[key]
-    }
+    },
   },
   mounted() {
     !this.getPageBySlugFromVUEX('homepage') &&
